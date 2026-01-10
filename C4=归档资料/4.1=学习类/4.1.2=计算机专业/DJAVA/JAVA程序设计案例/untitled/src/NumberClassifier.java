523c63598ae85fd68c448276b0f764f8a5558c1b
spencer <89265873@qq.com>
2026-01-10 10:49:30 +0800

    vault backup: 2026-01-10 10:49:30

--- C4=归档资料/4.1=学习类/4.1.2=计算机专业/DJAVA/JAVA程序设计案例/untitled/src/NumberClassifier.java
+++ C4=归档资料/4.1=学习类/4.1.2=计算机专业/DJAVA/JAVA程序设计案例/untitled/src/NumberClassifier.java
@@ -1,3 +1,14 @@
+import java.util.Scanner;
+
 public class NumberClassifier {
-    
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
+    if (i > 0) {
+        System.out.println("正数");
+    } else if (i < 0) {
+        System.out.println("负数");
+    } else
+    {
+        System.out.println("0");
+    }
 }

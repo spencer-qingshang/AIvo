1a5cd4c2d0cd1567f8062310504d6b706fd6f843
spencer <89265873@qq.com>
2026-01-10 10:52:39 +0800

    vault backup: 2026-01-10 10:52:39

--- C4=归档资料/4.1=学习类/4.1.2=计算机专业/DJAVA/JAVA程序设计案例/untitled/src/NumberClassifier.java
+++ C4=归档资料/4.1=学习类/4.1.2=计算机专业/DJAVA/JAVA程序设计案例/untitled/src/NumberClassifier.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 
 public class NumberClassifier {
-    Scanner sc = new Scanner(System.in);
-    int i = sc.nextInt();
-    if (i > 0) {
-        System.out.println("正数");
-    } else if (i < 0) {
-        System.out.println("负数");
-    } else
-    {
-        System.out.println("0");
+    public static void main(String[] args) { // 将执行逻辑放入main方法
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个整数："); // 提示用户输入
+        int i = sc.nextInt();
+
+        if (i > 0) {
+            System.out.println("这是一个正数");
+        } else if (i < 0) {
+            System.out.println("这是一个负数");
+        } else {
+            System.out.println("这是零");
+        }
+        sc.close(); // 关闭Scanner，释放资源
     }
 }
